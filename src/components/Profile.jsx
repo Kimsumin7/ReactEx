136228132ca9f74b8a81e008a2fc74d0fd7f4d5f
SooMin Kim <Kimsoominj@gmail.com>
2024-10-28 17:39:23 +0900

    10월 28일

---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Profile({image, name, title}) { //props를 지우고  {전달받고자 하는 키}를 넣으면 아래와 같이 props.image이렇게 안 써도 됨
+    return (
+        <div className='profile'>
+            <img 
+            className='photo' 
+            src={image} 
+            alt="avatar" 
+            />
+            <h1>{name}</h1>
+            <p>{title}</p>
+        </div>
+    );
+}
+
